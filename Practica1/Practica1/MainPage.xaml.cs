00e6e3a191138bbb2fff846ea4b8ceeb79c5866a
DonAntonio04 <berrellezantonio04@gmail.com>
2024-01-08 20:22:40 -0700

    Agregar archivos de proyecto.

---
+++ Practica1/Practica1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practica1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Refresh.IsRefreshing = true;
+            Thread.Sleep(200);
+            Refresh.IsRefreshing = false;
+        }
+
+    }
+}
